6d22e8382566fb157caa07c64c26d6d6537f2ee9
Tony <1801938676@qq.com>
2016-12-20 09:25:46 +0800

    Files test01~test09 renaming.
    Previous Version: test1~test9
    Latest Version: test01~test09

---
+++ test05.py
@@ -0,0 +1,17 @@
+name = 'Tony Niu'
+age = 15 #I am too young too simple.
+height = 182 #centimeters
+IQ = 250 #It is real, I swear.
+hairs = 'black'
+eyes = 'black'
+teeth = 'white' #Interesting right?
+#Data above is my personal profile.
+
+print "Let's talk about %s, which is my name, first." % name
+print "I live %d years now." % age
+print "I am %d centimeters tall." % height
+print "My IQ is %s." % IQ
+print "I must emphasize that, it is real, really." #Actually it is not:)
+print "I have got %s hairs and %s eyes." % (hairs, eyes)
+print "If I add %d, %s, and %d together, I will get %s." % (
+	age, height, IQ, age + height + IQ) 
---
+++ test07.py
@@ -0,0 +1,24 @@
+print "Mary has a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10  #What'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#Watch that comma at the end. Try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 +end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+#I got it, the comma and the end of a line is used to continue displaying the next line,
+#right after it.
+#Without a comma, this 2 lines will be displayed as 2 lines respectively.
